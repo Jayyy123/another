4585e4bf184684c6d3ee655122fcd662bd18eb2b
Jokeseph <josephofilii@gmail.com>
2022-05-03 04:38:52 +0100

    s

--- anotherrr/anotherrr/settings.py
+++ anotherrr/anotherrr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g^x=9h*nw+ip9lg4vezo-a*yi004ypn*h^f266qp5(wdmeio#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','anotherr.herokuapp.com']
 
 
 # Application definition
